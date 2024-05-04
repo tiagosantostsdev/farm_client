8aeda6d4d6fc4b213cc281d2ad104e33b12b77aa
Tiago Santos <tiagossantostsdev@gmail.com>
2024-05-04 17:41:32 +0100

    Add the spinner loading in confirm vendaPage.

--- src/components/modal/confrimVendas.tsx
+++ src/components/modal/confrimVendas.tsx
@@ -3,9 +3,11 @@ import { createVendasSchema, createVendasType } from "../Schema/vendasSchema";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useState } from "react";
 import { createVendas, updateVendas } from "../../services/vendasService";
+import { Spinner } from "../Spinner/Spinner";
 
 export const ConfirmVendas = (params: Record<string, any>) => {
   const [erro, setErro] = useState(false);
+  const [openSpinner, setOpenSpinner] = useState(false);
   const {
     register,
     reset,
@@ -32,9 +34,12 @@ export const ConfirmVendas = (params: Record<string, any>) => {
     const id = response.data;
     const troco: number = data.valor - total;
     const res = await updateVendas(id, data, total, troco);
+    setOpenSpinner(true);
     if (!res) {
+      setOpenSpinner(false);
       return console.log("Falha ao actualizar venda");
     }
+    setOpenSpinner(true);
     console.log(res);
     reset();
     location.href = "/vendas";
@@ -43,71 +48,75 @@ export const ConfirmVendas = (params: Record<string, any>) => {
 
   return (
     <>
-      <form
-        onSubmit={handleSubmit(sendForm)}
-        className="fixed top-1/3 left-1/3 flex p-2 gap-6 items-center flex-col flex-wrap rounded w-2/4 min-h-44 shadow shadow-gray-700 bg-gray-800 bg-opacity-80"
-      >
-        <fieldset className="grid grid-cols-2 place-items-center gap-2 w-full flex-wrap">
-          <span className="flex flex-col">
-            <label className="text-white" htmlFor="nomeCliente">
-              Nome do Cliente
-            </label>
-            <input
-              {...register("nomeCliente")}
-              className="p-1 w-full outline-none border border-gray-600 rounded-md"
-              type="text"
-              placeholder="Ex: Pedro João"
-              id="nomeCliente"
-            />
-            {errors.nomeCliente && (
-              <span className="text-red-500 text-xs">
-                {errors.nomeCliente.message}
-              </span>
-            )}
-          </span>
-          <span className="flex flex-col">
-            <label className="text-white" htmlFor="valor">
-              Valor
-            </label>
-            <input
-              {...register("valor", { valueAsNumber: true })}
-              className="p-1 w-full outline-none border border-gray-600 rounded-md"
-              type="number"
-              min={100}
-              placeholder="0"
-              id="valor"
-            />
-            {errors.valor && (
-              <span className="text-red-500 text-xs">
-                {errors.valor.message}
-              </span>
-            )}
-          </span>
-        </fieldset>
-        {erro && (
-          <span className="text-center text-red-500 text-xl">
-            Valor insuficiente
-          </span>
-        )}
-        <p className="text-white text-xl">
-          Total: <span>{params.total}</span>
-          <span className="text-sm">Kz</span>{" "}
-        </p>
-        <div className="flex gap-3 ">
-          <button
-            onClick={() => params.setOpen(false)}
-            className=" rounded font-bold text-white bg-red-500 p-1 px-2 hover:bg-red-700 transition-all"
-          >
-            Cancelar
-          </button>
-          <button
-            type="submit"
-            className=" rounded font-bold text-white bg-green-500 p-1 px-2 hover:bg-green-700 transition-all"
-          >
-            Confirmar
-          </button>
-        </div>
-      </form>
+      {openSpinner ? (
+        <Spinner />
+      ) : (
+        <form
+          onSubmit={handleSubmit(sendForm)}
+          className="fixed top-1/3 left-1/3 flex p-2 gap-6 items-center flex-col flex-wrap rounded w-2/4 min-h-44 shadow shadow-gray-700 bg-gray-800 bg-opacity-80"
+        >
+          <fieldset className="grid grid-cols-2 place-items-center gap-2 w-full flex-wrap">
+            <span className="flex flex-col">
+              <label className="text-white" htmlFor="nomeCliente">
+                Nome do Cliente
+              </label>
+              <input
+                {...register("nomeCliente")}
+                className="p-1 w-full outline-none border border-gray-600 rounded-md"
+                type="text"
+                placeholder="Ex: Pedro João"
+                id="nomeCliente"
+              />
+              {errors.nomeCliente && (
+                <span className="text-red-500 text-xs">
+                  {errors.nomeCliente.message}
+                </span>
+              )}
+            </span>
+            <span className="flex flex-col">
+              <label className="text-white" htmlFor="valor">
+                Valor
+              </label>
+              <input
+                {...register("valor", { valueAsNumber: true })}
+                className="p-1 w-full outline-none border border-gray-600 rounded-md"
+                type="number"
+                min={100}
+                placeholder="0"
+                id="valor"
+              />
+              {errors.valor && (
+                <span className="text-red-500 text-xs">
+                  {errors.valor.message}
+                </span>
+              )}
+            </span>
+          </fieldset>
+          {erro && (
+            <span className="text-center text-red-500 text-xl">
+              Valor insuficiente
+            </span>
+          )}
+          <p className="text-white text-xl">
+            Total: <span>{params.total}</span>
+            <span className="text-sm">Kz</span>{" "}
+          </p>
+          <div className="flex gap-3 ">
+            <button
+              onClick={() => params.setOpen(false)}
+              className=" rounded font-bold text-white bg-red-500 p-1 px-2 hover:bg-red-700 transition-all"
+            >
+              Cancelar
+            </button>
+            <button
+              type="submit"
+              className=" rounded font-bold text-white bg-green-500 p-1 px-2 hover:bg-green-700 transition-all"
+            >
+              Confirmar
+            </button>
+          </div>
+        </form>
+      )}
     </>
   );
 };
